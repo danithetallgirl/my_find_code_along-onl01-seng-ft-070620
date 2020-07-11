77f4dccb7f740838805a0f5b6c5d5801f992b729
Dani Bassett <dani@danithetallgirl.com>
2020-07-11 05:07:13 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,25 +2,8 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  block_returns = []
   while i < collection.length
-    block_returns << yield(collection[i])
-    i = i + 1
-  end
-  
-  if block_returns.include?(false)
-    false
-  else
-    true
-  end
-end
-
-def my_find(collection)
-  i = 0
-  while i < collection.length
-    if yield(collection[i])
-      return collection[i]
-    end
+    return collection[i] if yield(collection[i])
     i = i + 1
   end
 end
